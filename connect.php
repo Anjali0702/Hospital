4774779fa6e9f2a7ce247ee80e49488b49ffbb92
Anjali Panda <112181583+Anjali0702@users.noreply.github.com>
2023-05-11 21:27:58 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername="localhost";
-$username="root";
-$pass="";
-$conn=new mysqli($servername,$username,$pass);
-if($conn->connect_error){
-    die("connection error".$conn->connect_error);
-}
